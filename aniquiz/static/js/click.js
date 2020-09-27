6551c4b63a8f1d4e3e4db7ac9898699ccee8f1ad
LyK <lyk@LyK.LyK>
2020-09-27 11:16:49 +0200

    The js file click.js now change the border color of the answer selected. Plus set the radio button to true when the user is clicking on the answer.

--- aniquiz/static/js/click.js
+++ aniquiz/static/js/click.js
@@ -1,13 +1,30 @@
 var containersElts = document.getElementsByClassName("container");
+var list_color_border = []
+
+for (var i = 0; i < containersElts.length; i++) {
+    list_color_border.push(getComputedStyle(containersElts[i]).borderBottomColor);
+}
 
 function set_true_radio_button(e) {
-    // Get the element listened by the 
-    // event listener, and then move to
-    // the input node to change his value.
+    /*
+    Get the element listened by the 
+    event listener, and then move to
+    the input node to set his 
+    value to true.
+    Plus, change the border color of the
+    container selected into green.
+    */
     
+
     var target = e.currentTarget;
     var input_node = target.childNodes[1].childNodes[3];
     input_node.checked = true;
+
+    for (var i = 0; i < containersElts.length; i++) {
+        containersElts[i].style.borderColor = list_color_border[i];
+    }
+    target_style = getComputedStyle(target);
+    target.style.borderColor = "#00cc00";
 }
 
 for (var i = 0; i < containersElts.length; i++) {
